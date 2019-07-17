501d8b304fd7526e7abf39836672695c3fdf665b
Jonathan <joncomas@outlook.com>
2019-07-17 19:23:10 +0000

    DOE

--- api/family_datastructure.py
+++ api/family_datastructure.py
@@ -15,18 +15,24 @@ class Family:
         # example list of members
         self._members = [{
             "id": 1,
-            "first_name": "John"
+            "first_name": "John",
+            "lucky_num": 0,
+
         }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
+    def _generateLuckyNum(self):
+        return randint(1, 99)
+
     def add_member(self, member):
         # fill this method and update the return
         agregado = {
             "id": self._generateId(),
             "first_name": member["first_name"],
+            "lucky_num": self._generateLuckyNum(),
         }
         self._members.append(agregado)
         return agregado
@@ -34,8 +40,8 @@ class Family:
     def delete_member(self, id):
         # fill this method and update the return
         for x in self._members:
-            if x.id == id:
-                return self._members.remove(x)
+            if x["id"] == id:
+                self._members.remove(x)
 
 
     def update_member(self, id, member):
@@ -50,8 +56,10 @@ class Family:
     def get_member(self, id):
         # fill this method and update the return
         for x in self._members:
-            if x.id == id:
+            if x["id"] == id:
                 return x
+            else:
+                return("Not found")
 
     # this method is done, it returns a list with all the family members
 
